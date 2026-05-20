f7b5bcc0d7821fcfc80b5c66c4917d47955660cf
Heather Kropp <kroppheather@gmail.com>
2026-05-20 14:03:04 -0400

    Update img_process.R

--- img_process.R
+++ img_process.R
@@ -7,7 +7,7 @@ imgs = list.files(imgDir, pattern=".tif")
 imgs = imgs[-grep(".xml", imgs)]
 imgs = imgs[-grep(".ovr", imgs)]
 
-dimCrop <- 1000
+dimCrop <- 2200
 
 img_org <- list()
 img_crop <- list()
@@ -15,7 +15,7 @@ dims <- list()
 for(i in 1:length(imgs)){
   img_org[[i]] = rast(paste0(imgDir,"/",imgs[i]))
   dims[[i]] <- dim(img_org[[i]])[1:2]
-  img_crop[[i]] = img_org[[i]][dimCrop:dims[[i]][1]-dimCrop,dimCrop:dims[[i]][2]-dimCrop, drop=FALSE]
+  img_crop[[i]] = img_org[[i]][dimCrop:(dims[[i]][1]-dimCrop),dimCrop:(dims[[i]][2]-dimCrop), drop=FALSE]
 }
 
 plot(img_org[[1]], col=gray(1:100/100))
@@ -25,3 +25,12 @@ plot(img_org[[3]], col=gray(1:100/100))
 plot(img_crop[[1]], col=gray(1:100/100))
 plot(img_crop[[2]], col=gray(1:100/100))
 plot(img_crop[[3]], col=gray(1:100/100))
+
+
+dirSave = "K:/Environmental_Studies/hkropp/Private/aerial_40s_crop"
+for(i in 1:length(imgs)){
+  writeRaster(img_crop[[i]], paste0(dirSave,"/",imgs[i]))
+              
+}
+  
+
